a496301e776d68712f76a2c943f52fa4016ffa1a
Gabriel Diaz <gabriel.diaz@cap.edu.mx>
2022-09-05 10:46:12 -0500

    Tests on StudentsManagement. Unuseful tests files deleted.

--- tests/Feature/Api/StudentManagementTest.php
+++ tests/Feature/Api/StudentManagementTest.php
@@ -6,6 +6,8 @@ use App\Models\Tutor;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class StudentManagementTest extends TestCase
@@ -16,8 +18,11 @@ class StudentManagementTest extends TestCase
 
     public function setUp() : void
     {
+        parent::setUp();
+
+        Artisan::call('passport:install');
         User::factory(1)->create();
-        Tutor::factory(3)->create();
+        Tutor::factory(1)->create();
         $this->path = 'api/v1/students';
     }
 
@@ -26,19 +31,61 @@ class StudentManagementTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    /** @test */
+    public function test_must_create_a_new_student()
     {
         $user = User::first();
-        $response = $this->postJson('api/v1/login', [
-            'email' => $user->email,
-            'password' => 'password',
-            'device' => 'Any device'
+        $tutor = Tutor::first();
+
+        $response = $this->actingAs($user, 'api')
+        ->postJson($this->path, [
+            'name' => 'Pablo',
+            'lastname' => 'Larios',
+            'tutor_id' => $tutor->id
         ]);
 
-        $response->assertOk();
+        $response->assertStatus(201);
 
-        // $response = $this->get('/');
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'lastname',
+                'tutor' => ['id']
+            ]
+        ]);
 
-        // $response->assertStatus(200);
+        $response->assertJson(function(AssertableJson $json) use($tutor) {
+            
+            $json->whereAllType([
+                'data.id' => 'integer',
+                'data.name' => 'string',
+                'data.lastname' => 'string',
+                'data.tutor.id' => 'integer',
+            ])->etc();
+
+            $json->whereAll([
+                'data.name' => 'Pablo',
+                'data.lastname' => 'Larios',
+                'data.tutor.id' => $tutor->id
+            ])->etc();
+
+        });
+    }
+
+    /** @test */
+    public function test_must_be_invalid_the_form_where_is_incomplete_post()
+    {
+        $user = User::first();
+        $tutor = Tutor::first();
+
+        $response = $this->actingAs($user, 'api')
+        ->postJson($this->path, [
+            'lastname' => 'Larios',
+            'tutor_id' => $tutor->id
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertInvalid(['name']);
     }
 }
